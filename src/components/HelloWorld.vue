6480b4fb8a30284012ec4122021f856e8b13b5cc
Ethan Hoppe <32722553+EthanTheHuman@users.noreply.github.com>
2022-08-24 14:47:22 +1200

    fix 2

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -15,7 +15,7 @@ defineProps({
       You’ve successfully created a project with
       <a target="_blank" href="https://vitejs.dev/">Vite</a> +
       <a target="_blank" href="https://vuejs.org/">Vue 3</a> +
-      <router-link to="/multiverse">Multiverse Maker<router-link>.
+      <router-link to="/multiverse">Multiverse Maker</router-link>.
     </h3>
   </div>
 </template>
