4f04ad6261027e6917c15150cd9d873cc8f7b65f
Ethan Hoppe <32722553+EthanTheHuman@users.noreply.github.com>
2022-08-24 15:58:14 +1200

    Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref, computed } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/NavBar.vue'
 import TheWelcome from './components/TheWelcome.vue'
 import MultiverseCharacter from './components/MultiverseCharacter.vue'
 
@@ -22,11 +23,7 @@ const currentView = computed(() => {
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    </Navbar>
   </header>
 
   <main>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,43 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="navbar">
+    TRUE
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.navbar {
+    width: 100%;
+    background-color: white;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
