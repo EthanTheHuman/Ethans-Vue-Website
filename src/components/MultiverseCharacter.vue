82dd0d6a972378128b33d76f0e2b54da0f63acee
Ethan Hoppe <32722553+EthanTheHuman@users.noreply.github.com>
2022-08-28 20:48:30 +1200

    Swap gradient

--- src/components/MultiverseCharacter.vue
+++ src/components/MultiverseCharacter.vue
@@ -116,8 +116,8 @@ h3 {
   margin: 0px;
   width: 100%;
   text-align: center;background: rgb(255,0,0);
-  background: linear-gradient(0deg, rgba(255,0,0,0.1) 0%, rgba(255,0,0,1) 100%);
-  border: 1px solid white;
+  background: linear-gradient(0deg, rgba(255,0,0,1) 0%, rgba(255,0,0,0.1) 100%);
+  border: 1px solid red;
 }
 
 .row {
