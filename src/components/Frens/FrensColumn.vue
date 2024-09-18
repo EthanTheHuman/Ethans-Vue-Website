fda7f4de1e9ad1e555d24457b784f68fdcd48525
Ethan Hoppe <EthanHoppe053@gmail.com>
2024-09-18 15:32:04 +1200

    Initial logic

---
+++ src/components/Frens/FrensColumn.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <h3>Friends List</h3>
+      <div v-for="friend in friends" :key="friend.Name">
+        <button @click="toggle(friend)">
+          {{ friend.Name }}
+        </button>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ['friends'],
+    methods: {
+      toggle(friend) {
+        this.$emit('toggleFriend', friend);
+      }
+    }
+  };
+  </script>
+  
